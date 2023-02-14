09e586aa83ac102e22b2be9e7578fe3107742e32
Istamovashakhzoda <117287001+Istamovashakhzoda@users.noreply.github.com>
2023-02-14 18:17:53 +0300

    Add files via upload

---
+++ for operation/script.js
@@ -0,0 +1,43 @@
+"use script"
+
+/*let arry=[
+  [12,12,["went","go","gone"],13,45],
+  ["went","go","gone"]
+  ["html","css","scss"]
+]
+console.log(arry[0][3]);
+const obc={
+   lastN:"Surname",
+   location:"your orign",
+   password:"1q2w3e4r",
+   year:"your birth year",
+
+   country:{
+    davlat:"UZB",
+    tuman:"tuman"
+   }
+}
+console.log(obc);
+console.log(obc.country.tuman);*/
+
+//for section
+/*let arr=[1,2,3,4,5,6]
+ for(let i=0; i<=5; i++){
+   console.log(arr[i]);
+ }*/
+//console.log(arr);
+
+//1-MASALA
+/*for ( let i=0; i<11; i++){
+console.log(i);
+}*/
+
+//2-MASALA
+/*for(let a=0; a<10;a++){
+  console.log(a);
+}*/
+
+//3-MASALA
+/*for(let i=0; i<10; i++){
+  console.log(i);
+}*/
